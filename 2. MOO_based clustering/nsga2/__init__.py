4c2aa0099ef192f2c40df1e34a1b7a0b80ea1626
Pratik <dhanachandraningthoujamcse@gmail.com>
2019-02-06 12:45:27 +0530

    MOO updated

--- 2. MOO_based clustering/nsga2/__init__.py
+++
@@ -1,7 +0,0 @@
-def seq(start, stop, step=1):
-    n = int(round((stop - start)/float(step)))
-    if n > 1:
-        print
-        return([start + step*i for i in range(n+1)])
-    else:
-        return([])
